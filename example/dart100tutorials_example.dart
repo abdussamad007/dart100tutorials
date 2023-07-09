5bfd14569b4738e5ce5777253dce88b3af531eb5
abdus <abdussamad007@gmail.com>
2023-07-09 22:54:30 +0530

    Initial version of the commit

---
+++ example/dart100tutorials_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart100tutorials/dart100tutorials.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dart100tutorials_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart100tutorials/dart100tutorials.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
